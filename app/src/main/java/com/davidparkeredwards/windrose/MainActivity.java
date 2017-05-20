9b903290a515c276c52ce02056ad1c83fdfd6f5b
588732 <davidedwards@C02S2426G8WM.fios-router.home>
2017-05-19 23:19:08 -0400

    Connected to Firebase Database and location manager

--- app/src/main/java/com/davidparkeredwards/windrose/MainActivity.java
+++ app/src/main/java/com/davidparkeredwards/windrose/MainActivity.java
@@ -1,14 +1,26 @@
 package com.davidparkeredwards.windrose;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +29,59 @@ public class MainActivity extends AppCompatActivity {
 
         FirebaseAuth auth = FirebaseAuth.getInstance();
         if (auth.getCurrentUser() != null) {
-            Log.i(TAG, "onCreate: User is signed in as " + auth.getCurrentUser().toString());
+            Log.i(TAG, "onCreate: User is signed in as " + auth.getCurrentUser().getDisplayName());
+            Location location = getLocation();
+            if (location != null) {
+                Log.i(TAG, "onCreate: Location: " + location.toString());
+                saveLocationToFirebase(location);
+            } else {
+                Log.i(TAG, "onCreate: Location is null");
+            }
         } else {
             Log.i(TAG, "onCreate: User is not signed in");
+            startActivityForResult(
+                    // Get an instance of AuthUI based on the default app
+                    AuthUI.getInstance().createSignInIntentBuilder().build(),
+                    RC_SIGN_IN);
         }
+    }
 
+    protected Location getLocation() {
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                "android.permission.ACCESS_FINE_LOCATION");
 
+        if(permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+            String locationProvider = LocationManager.GPS_PROVIDER;
+
+            Location lastKnownLocation = locationManager.getLastKnownLocation(locationProvider);
+            return lastKnownLocation;
+        }
+        return null;
+    }
+
+    protected void saveLocationToFirebase(Location location) {
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("location");
+
+        myRef.setValue(location.toString());
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
     }
 }
